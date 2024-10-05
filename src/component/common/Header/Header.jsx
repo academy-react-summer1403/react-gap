28f1abe1955594a461982e193abbe24596f93090
atiye <atiyekaviyanii78@gmail.com>
2024-10-05 23:48:16 +0330

    Header

--- src/component/common/Header/Header.jsx
+++ src/component/common/Header/Header.jsx
@@ -4,7 +4,7 @@ import { Fragment } from 'react'
 const Header = () => {
   return (
    <Fragment>
-    <div className='border-2 border-black w-screen h-32' >
+    <div className='border-2 border-red-500 w-screen h-28' >
  
 
     </div>
