f52c32106066af0f76c9d1781728c3531d9a72b6
atiye <atiyekaviyanii78@gmail.com>
2024-10-05 15:52:32 +0330

    structor

---
+++ src/component/common/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Fragment } from 'react'
+
+const Header = () => {
+  return (
+   <Fragment>
+    <div className='Header' style={{border:'1px solid red', width:'100%',}} >
+
+
+    </div>
+
+   </Fragment>
+  )
+}
+
+export default Header
