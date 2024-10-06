d43994ace562625bc6ba4a827a5528cdcd2ad313
mamad <mamda@gmail.com>
2024-10-06 12:26:43 +0330

    Edit

--- src/component/LandingHolder/Landing.jsx
+++ src/component/LandingHolder/Landing.jsx
@@ -3,16 +3,9 @@ import React from 'react'
 const Landing = () => {
   return (
     <div>
-<<<<<<< HEAD
+
     
 
-=======
-   <h1>
-    landing
-
-   
-   </h1>
->>>>>>> develope
     </div>
   )
 }
