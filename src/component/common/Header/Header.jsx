53ae5d81b619ed66f296b391c035aa6f35e9d562
mamad <mamda@gmail.com>
2024-10-06 12:26:49 +0330

    c

--- src/component/common/Header/Header.jsx
+++ src/component/common/Header/Header.jsx
@@ -1,33 +1,31 @@
 import React from 'react'
 import { Fragment } from 'react'
-import Login from '../../../screens/Login/Login'
 
 const Header = () => {
-  return (
-   <Fragment>
-    <div className='flex justify-center flex-row border-2 border-red-100-500 w-screen h-28 bg-gradient-to-tr ' >
-      <div> <img src="" alt="" /> </div>
-      <h1> آکادمی اچ وان </h1>
-       <button> صفحه اصلی </button>
-       <div> دوره ها </div>
-       <div> مقالات </div>
-       <div> درباره ما </div>
-       
-      <div>
-        <img src="" alt="" />
-      </div>
+    return (
+    <Fragment>
+      <div className='flex justify-center flex-row border-2 border-red-100-500 w-screen h-28 bg-gradient-to-tr ' >
+        <div> <img src="" alt="" /> </div>
+        <h1> آکادمی اچ وان </h1>
+        <button> صفحه اصلی </button>
+        <div> دوره ها </div>
+        <div> مقالات </div>
+        <div> درباره ما </div>
+        
+        <div>
+          <img src="" alt="" />
+        </div>
 
-<<<<<<< HEAD
-     <Login/>
-=======
+
+    
+
       <button> ثبت نام / ورود </button>
- 
-     
 
->>>>>>> develope
+
+
     </div>
 
-   </Fragment>
+</Fragment>
   )
 }
 
