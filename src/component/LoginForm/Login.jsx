7bcd722332a605f35f76bbd699adc85138b4b49f
mamad <mamda@gmail.com>
2024-10-06 19:25:24 +0330

    edit login

--- src/component/LoginForm/Login.jsx
+++ src/component/LoginForm/Login.jsx
@@ -1,12 +1,36 @@
+
 import React from 'react'
-
+import { FaMobileAlt } from "react-icons/fa";
+import { IoKeyOutline } from "react-icons/io5";
 const Login = () => {
-  return (
-    <div>
 
-        LoginPage
-      
-    </div>
+
+  return (
+    
+    <div className='wrapper'> 
+      <form action="">
+        <h1>Login</h1>
+        <div className='input-box'>
+        <input type='text' placeholder='شماره همراه' required />
+        <FaMobileAlt />
+        </div>
+        <div className='input-box'>
+        <input type='password' placeholder='رمز عبور' required />
+        <IoKeyOutline />
+        </div>
+        <div className='remember-forget'>
+
+          <label><input type="checkbox" /> مرا به خاطر بسپارید</label>
+            <br/>
+          <a href='#'>فراموشی رمز</a>
+
+        </div>
+        <button type='submit'>Login</button>
+        <div className="register-link">
+          <p>یک حساب کاربری ایجاد کنید <a href='#'>Register</a></p>
+        </div>
+      </form>
+     </div>
   )
 }
 
--- src/screens/Landing/index.jsx
+++ src/screens/Landing/index.jsx
@@ -13,6 +13,7 @@ const Landing = () => {
       <HeroSection/>
 
       <Footer/>
+      
     
 
       
