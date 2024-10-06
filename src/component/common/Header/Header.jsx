bc07a92bca0c0838b803d8e3e71f71b4f0cdac4b
atiye <atiyekaviyanii78@gmail.com>
2024-10-06 10:07:19 +0330

    Header

--- src/component/common/Header/Header.jsx
+++ src/component/common/Header/Header.jsx
@@ -4,8 +4,20 @@ import { Fragment } from 'react'
 const Header = () => {
   return (
    <Fragment>
-    <div className='border-2 border-red-500 w-screen h-28' >
+    <div className='flex justify-center flex-row border-2 border-red-100-500 w-screen h-28 bg-gradient-to-tr ' >
+      <div> <img src="" alt="" /> </div>
+      <h1> آکادمی اچ وان </h1>
+       <button> صفحه اصلی </button>
+       <div> دوره ها </div>
+       <div> مقالات </div>
+       <div> درباره ما </div>
+      <div>
+        <img src="" alt="" />
+      </div>
+
+      <button> ثبت نام / ورود </button>
  
+    
 
     </div>
 
