29ebd9e2abc29afb6815a0e704e9fa4b6a2cacf5
mamad <mamda@gmail.com>
2024-10-07 12:02:05 +0330

    x

--- src/component/LoginForm/Login.jsx
+++ src/component/LoginForm/Login.jsx
@@ -6,36 +6,54 @@ const Login = () => {
 
 
   return (
-    
-    <div className='flex justify-center border-2 w-[1539px] h-[557px] font-inter'>
-        <div className='flex justify-center border-2 w-[800px] '> 
+    <div className=' flex justify-center  w-screen h-[500px] m-10'>
+    <div className='flex border-2 w-[800px] h-[500px] rounded-lg'> 
+      
+        <div className='flex justify-center border-2 w-[400px] h-[500px] bg-[#FBF6F6;] '>
+          <form action="">
+          <h1 >ورود به سیستم</h1>
+          <div className='input-box'>
 
-        <form action="">
-          <h1>Login</h1>
-          <div className='input-box'>
-          <input type='text' placeholder='شماره همراه' required />
-          <FaMobileAlt />
+          <input  className=' rounded-lg  w-[340px] h-[35px] mt-4 i' type='text' placeholder='شماره همراه' required  />
+          <FaMobileAlt  className='icoc'/>
           </div>
-          <div className='input-box'>
-          <input type='password' placeholder='رمز عبور' required />
-          <IoKeyOutline />
+          <div className='input-box '>
+          <input className='border-2 rounded-lg  w-[340px] h-[35px] mt-4' type='password' placeholder='رمز عبور' required />
+          <IoKeyOutline  className='icoc'/>
           </div>
           <div className='remember-forget'>
 
             <label><input type="checkbox" /> مرا به خاطر بسپارید</label>
               <br/>
+              
             <a href='#'>فراموشی رمز</a>
 
           </div>
           <button type='submit'>Login</button>
           <div className="register-link">
-            <p>یک حساب کاربری ایجاد کنید <a href='#'>Register</a></p>
+
+          <button className=' w-[340px] h-[40px] bg-[#158B68;] text-white rounded-lg'>تایید </button>
+          <br/>
+          <button className='w-[340px] h-[40px] bg-[#158B68;] text-white rounded-lg mt-4'> بازگشت به صفحه اصلی</button>
+
+                        <br />
+            <a href='#'>یک حساب کاربری ایجاد کنید </a>
+
           </div>
         </form>
+        </div>
+
+        <div className='border-2 w-[400px] h-[500px] bg-[#A4F6DE;]'>
+          <img src="" alt="" />
+        </div>
+
+
+      
       </div>
+
       </div>
-    
-    )
-  }
+
+  )
+}
 
 export default Login
