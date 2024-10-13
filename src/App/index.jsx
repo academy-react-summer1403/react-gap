0c2f8174f8f3ae9fe8eae1afacc525d7c3d336ce
mamad <mamda@gmail.com>
2024-10-13 19:16:47 +0330

    login2

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -109,7 +109,7 @@ const index = () => {
   ])
 
   return (
-    <div>
+    <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden  border-2 border-red-500'>
       <RouterProvider router={Routers} />
     </div>
 
