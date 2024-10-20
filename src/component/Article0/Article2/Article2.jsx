c273b604cdfe14c20031a766a42770072f8d3961
mamad <mamda@gmail.com>
2024-10-20 11:18:22 +0330

    c

--- src/component/Article0/Article2/Article2.jsx
+++ src/component/Article0/Article2/Article2.jsx
@@ -2,8 +2,8 @@
 
     const Article2 = () => {
     return (
-        <div>
-            Article2
+        <div className='border-2 border-red-400 w-96 h-52'>
+            
         
         </div>
     )
