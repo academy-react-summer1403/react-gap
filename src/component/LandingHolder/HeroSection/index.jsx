cb7e12e568ab8022d9aee16bdbedf0c66eae2960
atiye <atiyekaviyanii78@gmail.com>
2024-10-24 23:09:50 +0330

    DarkMode

--- src/component/LandingHolder/HeroSection/index.jsx
+++ src/component/LandingHolder/HeroSection/index.jsx
@@ -4,12 +4,12 @@ import NavBar from "../../common/NavBarHero/NavBar";
 
 const index = () => {
   return (
-    <div className="flex justify-start h-[700px] bg-gradient-to-r from-[#A4F6DE] to-[#FFFFFF] dark:from-[#22445D] dark:to-[#FFFFFF] ">
+    <div className="flex justify-start h-[700px] bg-gradient-to-r from-[#A4F6DE] to-[#FFFFFF] dark:bg-slate-800 ">
       <div className="  w-[800px] h-[400px] relative top-32 left-1 text-center lg:text-right ">
-        <h1 className="font-inter xl:text-6xl text-[#21394B] mx-16 md:text-4xl text-4xl sm:text-4xl">
+        <h1 className="font-inter xl:text-6xl text-[#21394B] mx-16 md:text-4xl text-4xl sm:text-4xl dark:text-white">
           آموزشگاه اچ وان{" "}
         </h1>
-        <h3 className="m-14 text-[#22445D;] text-xl font-normal">
+        <h3 className="m-14 text-[#22445D;] text-xl font-normal dark:text-white">
           لورم ایپسوم محبوب ترین و استانداردترین متن ساختگی است که توسط توسعه
           دهندگان وب، تایپوگراف ها و طراحان استفاده می شود. تکه های لاتین متن
           نشان میدهد که یک پروژه در حال توسعه است. لورم اپسوم فقط برای توسعه
