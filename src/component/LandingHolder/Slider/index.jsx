16b715171361b2a4a391d5602a92f5be6db3f623
atefeh <atefeh@gmail.com>
2024-10-24 23:05:07 +0330

    new edit

--- src/component/LandingHolder/Slider/index.jsx
+++ src/component/LandingHolder/Slider/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -20,8 +20,16 @@ import { NavLink } from "react-router-dom";
 import { AiOutlineLike } from "react-icons/ai";
 import { AiOutlineDislike } from "react-icons/ai";
 import { FaRegStar } from "react-icons/fa";
+import axios from "axios";
+import { Rate } from 'antd';
+
+
+
+
+
 const index = () => {
   const [ListSlider, setListSlider] = useState([
+    
     {
       id: 1,
       pic: "iconslider.png",
@@ -36,6 +44,18 @@ const index = () => {
     },
   ]);
 
+  const [TopCoursesList, setTopCoursesList] = useState(null);
+
+  const getTopCourses = async () =>{
+    const res = await axios.get('https://classapi.sepehracademy.ir/api/Home/GetCoursesTop?Count=5')
+    setTopCoursesList(res.data)
+  }
+
+
+  useEffect(() => {
+    getTopCourses()
+  }, []);
+
   return (
     <div className="h-[1100px]">
       <div className=" h-[150px]  text-center m-20">
@@ -47,8 +67,7 @@ const index = () => {
         </p>
 
         <div className=" w-[98%] mx-auto m-10">
-          {ListSlider.map((item) => {
-            return (
+
               <div className=" h-[900px]">
                 <Swiper
                   className="h-[700px] relative top-20 "
@@ -62,279 +81,72 @@ const index = () => {
                   onSwiper={(swiper) => console.log(swiper)}
                   onSlideChange={() => console.log("slide change")}
                 >
-                  <SwiperSlide className="w-[98%]">
-                  <div className="card bg-base-100 w-[100%] h-[100%] shadow-xl">
-                      <figure className="px-10 pt-10">
-                        <img
-                          src="./slidicon.png"
-                          alt="Shoes"
-                          className="rounded-xl"
-                        />
-                      </figure>
 
-                      <div className="card-body items-center text-center">
-                        <div className="card-actions ml-52">
-                          <button className="btn btn-primary bg-[#12926C;] mr-7">
+                  {TopCoursesList?.map(item=>{
+                    return(
+                      <SwiperSlide className="w-[98%]">
+                      <div className="card bg-base-100 w-[100%] h-[100%] shadow-xl">
+                        <figure className="px-10 pt-10">
+                          <img
+                            src={item.tumbImageAddress}
+                            alt="Shoes"
+                            className="rounded-xl"
+                          />
+                        </figure>
+  
+                        <div className="card-body items-center text-center">
+                          <div className="card-actions ml-52">
+                            <button className="btn btn-primary bg-[#12926C;] mr-7">
+                              {" "}
+                              وضعیت دوره{" "}
+                            </button>
+  
+                            <div className="card-actions relative right-52 -top-14">
+                              <div className="badge badge-outline p-5 hover:bg-[#41d3a7]  hover:border-none">
+                                <AiOutlineLike />
+                              </div>
+                              <div className="badge badge-outline p-5 hover:bg-[#41d3a7]  hover:border-none">
+                                <AiOutlineDislike />
+                              </div>
+                              <div className="badge badge-outline p-5 hover:bg-[#41d3a7]  hover:border-none">
+                                <FaRegStar />
+                              </div>
+                            </div>
+                          </div>
+                          <h2 className="card-title text-2xl">
+                              {item.title}
+                          </h2>
+                          <p className="text-right">
                             {" "}
-                            وضعیت دوره{" "}
-                          </button>
-
-                          <div className="card-actions relative right-52 -top-14">
-                            <div className="badge badge-outline p-5 hover:bg-[#41d3a7]  hover:border-none">
-                              <AiOutlineLike />
-                            </div>
-                            <div className="badge badge-outline p-5 hover:bg-[#41d3a7]  hover:border-none">
-                              <AiOutlineDislike />
-                            </div>
-                            <div className="badge badge-outline p-5 hover:bg-[#41d3a7]  hover:border-none">
-                              <FaRegStar />
+                              {item.describe}
+                          </p>
+                          <div>
+                            <div className="">
+ 
+                            <Rate allowHalf defaultValue={item.courseRate} />
                             </div>
                           </div>
-                        </div>
-                        <h2 className="card-title text-2xl">آشنایی با برنامه نویسی با وردپرس</h2>
-                        <p className="text-right"> لورم ایپسوم محبوب ترین و استانداردترین متن ساختگی است که توسط توسعه دهندگان وب، تایپوگراف ها و طراحان استفاده می شود </p>
-                        <div>
-                        <div className="rating ml-72">
-  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
-  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" defaultChecked />
-  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
-  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
-  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
-</div>
-                        </div>
-                        <div className="flex flex-wrap justify-between gap- border-t-2 border-green-900 w-[90%]">
-                          <h1 className="text-[green]">هزینه تمام دوره</h1>
-                          <h2 className="text-[red]">3000000 هزار تومان</h2>
-
-                        </div>
-                        <div className="card-actions">
-                          <button className="btn btn-primary bg-[#12926C;] w-60 p-7 leading-4">
-                            الان ثبت نام کن{" "}
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-
-                    {/* <div className="border-2 shadow-2xl w-[100%] h-[700px] rounded-3xl ">
-                      <div className="h-[200px] mr-32 relative bottom-28 ">
-                        <img
-                          className="w-[100%] absolute"
-                          src={item.pic}
-                          alt=""
-                        />
-                      </div>
-
-                      <div className=" w-[95%] h-[80%] relative bottom-16 mx-auto">
-                        <div>
-                          <NavLink to={"/courses"}>
-                            <button className="w-[150px] h-[50px] rounded-xl bg-[#BFF4E4;] text-[#158B68;] text-xl relative left-36  hover:scale-75 duration-75 ">
-                            {item.button}
-                          </button> 
-                          </NavLink>
-                         
-                        </div>
-
-                        <div className="w-[130px] h-[40px] relative bottom-11 right-72 ">
-                          <img src={item.Like} alt="" />
-                        </div>
-                        <div className="text-3xl font-semibold relative bottom-6  ">
-                          {" "}
-                          <h1>{item.title}</h1>{" "}
-                        </div>
-                        <div className="w-[400px] h-[50px] relative right-7">
-                          <img src={item.jun} alt="" />
-                        </div>
-                        <div className="w-[400px] h-[50px] text-right text-[#158B68;] relative right-7">
-                          <h2>{item.mit}</h2>
-                        </div>
-                        <div className="w-[400px] h-[120px] text-right relative top-3 right-4">
-                          <p>{item.Desc}</p>
-                        </div>
-                        <div className="w-[430px] border-t-2 border-[#158B68;] relative right-3"></div>
-
-                        <div className="flex gap-36 w-[450px] h-[50px]">
-                          <div className="w-[200px] text-lg text-right leading-10 text-[#158B68;]">
-                            <h2>: هزینه دوره</h2>
+                          <div className="flex flex-wrap justify-between gap- border-t-2 border-green-900 w-[90%]">
+                            <h1 className="text-[green]">هزینه تمام دوره</h1>
+                            <h2 className="text-[red]">3000000 هزار تومان</h2>
                           </div>
-                          <div className="w-[300px] text-[red] text-2xl">
-                            <h2>3.000.000 تومان</h2>
+                          <div className="card-actions">
+                            <button className="btn btn-primary bg-[#12926C;] w-60 p-7 leading-4">
+                              الان ثبت نام کن{" "}
+                            </button>
                           </div>
                         </div>
-                        <div className=" w-[300px] h-[70px] relative top-10 right-14 bg-[#5BE1B9;] rounded-xl text-2xl text-[#fff] leading-[60px]  hover:scale-75 duration-75">
-                          <NavLink to={"/Login"}>
-                            <button>{item.Sign}</button>
-                          </NavLink>
-                        </div>
                       </div>
-                    </div> */}
-                  </SwiperSlide>
-                  <SwiperSlide className="w-[98%]">
-                  <div className="card bg-base-100 w-[100%] h-[100%] shadow-xl">
-                      <figure className="px-10 pt-10">
-                        <img
-                          src="./slidicon.png"
-                          alt="Shoes"
-                          className="rounded-xl"
-                        />
-                      </figure>
+  
+                      
+                    </SwiperSlide>
+                    )
+                  })}
 
-                      <div className="card-body items-center text-center">
-                        <div className="card-actions ml-52">
-                          <button className="btn btn-primary bg-[#12926C;] mr-7">
-                            {" "}
-                            وضعیت دوره{" "}
-                          </button>
 
-                          <div className="card-actions relative right-52 -top-14">
-                            <div className="badge badge-outline p-5 hover:bg-[#41d3a7]  hover:border-none">
-                              <AiOutlineLike />
-                            </div>
-                            <div className="badge badge-outline p-5 hover:bg-[#41d3a7]  hover:border-none">
-                              <AiOutlineDislike />
-                            </div>
-                            <div className="badge badge-outline p-5 hover:bg-[#41d3a7]  hover:border-none">
-                              <FaRegStar />
-                            </div>
-                          </div>
-                        </div>
-                        <h2 className="card-title text-2xl">آشنایی با برنامه نویسی با وردپرس</h2>
-                        <p className="text-right"> لورم ایپسوم محبوب ترین و استانداردترین متن ساختگی است که توسط توسعه دهندگان وب، تایپوگراف ها و طراحان استفاده می شود </p>
-                        <div>
-                        <div className="rating ml-72">
-  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
-  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" defaultChecked />
-  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
-  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
-  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
-</div>
-                        </div>
-                        <div className="flex flex-wrap justify-between gap- border-t-2 border-green-900 w-[90%]">
-                          <h1 className="text-[green]">هزینه تمام دوره</h1>
-                          <h2 className="text-[red]">3000000 هزار تومان</h2>
-
-                        </div>
-                        <div className="card-actions">
-                          <button className="btn btn-primary bg-[#12926C;] w-60 p-7 leading-4">
-                            الان ثبت نام کن{" "}
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                    
-                  </SwiperSlide>
-                  <SwiperSlide className="w-[98%]">
-                  <div className="card bg-base-100 w-[100%] h-[100%] shadow-xl">
-                      <figure className="px-10 pt-10">
-                        <img
-                          src="./slidicon.png"
-                          alt="Shoes"
-                          className="rounded-xl"
-                        />
-                      </figure>
-
-                      <div className="card-body items-center text-center">
-                        <div className="card-actions ml-52">
-                          <button className="btn btn-primary bg-[#12926C;] mr-7">
-                            {" "}
-                            وضعیت دوره{" "}
-                          </button>
-
-                          <div className="card-actions relative right-52 -top-14">
-                            <div className="badge badge-outline p-5 hover:bg-[#41d3a7]  hover:border-none">
-                              <AiOutlineLike />
-                            </div>
-                            <div className="badge badge-outline p-5 hover:bg-[#41d3a7]  hover:border-none">
-                              <AiOutlineDislike />
-                            </div>
-                            <div className="badge badge-outline p-5 hover:bg-[#41d3a7]  hover:border-none">
-                              <FaRegStar />
-                            </div>
-                          </div>
-                        </div>
-                        <h2 className="card-title text-2xl">آشنایی با برنامه نویسی با وردپرس</h2>
-                        <p className="text-right"> لورم ایپسوم محبوب ترین و استانداردترین متن ساختگی است که توسط توسعه دهندگان وب، تایپوگراف ها و طراحان استفاده می شود </p>
-                        <div>
-                        <div className="rating ml-72">
-  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
-  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" defaultChecked />
-  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
-  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
-  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
-</div>
-                        </div>
-                        <div className="flex flex-wrap justify-between gap- border-t-2 border-green-900 w-[90%]">
-                          <h1 className="text-[green]">هزینه تمام دوره</h1>
-                          <h2 className="text-[red]">3000000 هزار تومان</h2>
-
-                        </div>
-                        <div className="card-actions">
-                          <button className="btn btn-primary bg-[#12926C;] w-60 p-7 leading-4">
-                            الان ثبت نام کن{" "}
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                    
-                  </SwiperSlide>
-                  <SwiperSlide className="w-[98%]">
-                  <div className="card bg-base-100 w-[100%] h-[100%] shadow-xl">
-                      <figure className="px-10 pt-10">
-                        <img
-                          src="./slidicon.png"
-                          alt="Shoes"
-                          className="rounded-xl"
-                        />
-                      </figure>
-
-                      <div className="card-body items-center text-center">
-                        <div className="card-actions ml-52">
-                          <button className="btn btn-primary bg-[#12926C;] mr-7">
-                            {" "}
-                            وضعیت دوره{" "}
-                          </button>
-
-                          <div className="card-actions relative right-52 -top-14">
-                            <div className="badge badge-outline p-5 hover:bg-[#41d3a7]  hover:border-none">
-                              <AiOutlineLike />
-                            </div>
-                            <div className="badge badge-outline p-5 hover:bg-[#41d3a7]  hover:border-none">
-                              <AiOutlineDislike />
-                            </div>
-                            <div className="badge badge-outline p-5 hover:bg-[#41d3a7]  hover:border-none">
-                              <FaRegStar />
-                            </div>
-                          </div>
-                        </div>
-                        <h2 className="card-title text-2xl">آشنایی با برنامه نویسی با وردپرس</h2>
-                        <p className="text-right"> لورم ایپسوم محبوب ترین و استانداردترین متن ساختگی است که توسط توسعه دهندگان وب، تایپوگراف ها و طراحان استفاده می شود </p>
-                        <div>
-                        <div className="rating ml-72">
-  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
-  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" defaultChecked />
-  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
-  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
-  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
-</div>
-                        </div>
-                        <div className="flex flex-wrap justify-between gap- border-t-2 border-green-900 w-[90%]">
-                          <h1 className="text-[green]">هزینه تمام دوره</h1>
-                          <h2 className="text-[red]">3000000 هزار تومان</h2>
-
-                        </div>
-                        <div className="card-actions">
-                          <button className="btn btn-primary bg-[#12926C;] w-60 p-7 leading-4">
-                            الان ثبت نام کن{" "}
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                    
-                  </SwiperSlide>
-                  ...
                 </Swiper>
               </div>
-            );
-          })}
+
         </div>
       </div>
       <NavLink to={"/News"}>
--- src/component/common/Header/Header.jsx
+++ src/component/common/Header/Header.jsx
@@ -12,14 +12,14 @@ const Header = () => {
 
   return (
     <Fragment>
-      <div className="flex justify-between   h-24 bg-gradient-to-r from-[#A4F6DE] to-[#FFFFFF] dark:from-[#22445D] dark:to-[#FFFFFF] ">
+      <div className="flex justify-between  border-2 h-24 bg-gradient-to-r from-[#f2f8f2]  to-[rgb(242,248,242)] dark:from-[#020506] dark:to-[#FFFFFF] ">
         <Link to="/" className="flex  gap-2  w-60 h-20 m-6">
-          <div className=" w-[50px] h-[50px]">
-            <img src="./h11.png" className="" />
+          <div className=" w-[150px] h-[100px] relative bottom-5 right-9">
+            <img src="./logoheader.png" className="" />
           </div>
 
-          <h1 className="text-[#22445D;] font-inter text-2xl m-3">
-            آکادمی اچ وان
+          <h1 className="text-[#22445D;] font-inter text-[14px] m-3">
+           
           </h1>
         </Link>
 
@@ -50,8 +50,8 @@ const Header = () => {
 
 
         <div className="h-9 m-10 relative right-48">
-          <img src="./search.png" alt="" />
-
+          {/* <img src="./search.png" alt="" /> */}
+{/* 
     <label class="relative block">
   <span class="sr-only">Search</span>
   <span class="absolute inset-y-0 left-0 flex items-center pl-2">
@@ -60,10 +60,26 @@ const Header = () => {
     <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-full
     border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none
       focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-  placeholder="Search for anything..." type="text" name="search"/>
-  </label>
+  placeholder="هرچی میخوای جستجو کن..." type="text" name="search"/>
+  </label> */}
+
+
+<label className="input input-bordered flex items-center gap-2 relative left-20 bottom-4">
+  <input type="text" className="grow" placeholder="هرچی می‌خوای اینجا جستجو کن" />
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 16 16"
+    fill="currentColor"
+    className="h-4 w-4 opacity-70">
+    <path
+      fillRule="evenodd"
+      d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+      clipRule="evenodd" />
+  </svg>
+</label>
 
           </div>
+
 
 
 
@@ -98,7 +114,7 @@ const Header = () => {
           </div>
 
           <div className="m-6"><NavLink to={"/Login"}>
-            <button className="hover:text-[#158B68;] w-[150px] h-[57px] bg-[#00DF9D;] rounded-lg text-[#22445D;] font-inter relative left-6  hover:scale-75 duration-75">
+            <button className="hover:text-[#5c7158] w-[120px] h-[47px] bg-[#dfe3de] rounded-lg text-[#22445D;] font-inter relative left-6  hover:scale-75 duration-75">
               {" "}
               ورود / ثبت نام
               
