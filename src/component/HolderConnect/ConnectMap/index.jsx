b3a6559de99913902cc52461e84aade9278e317b
atiye <atiyekaviyanii78@gmail.com>
2024-11-02 11:37:33 +0330

    mapadreess

--- src/component/HolderConnect/ConnectMap/index.jsx
+++ src/component/HolderConnect/ConnectMap/index.jsx
@@ -3,7 +3,7 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 const Map = () => {
-  const position = [51.505, -0.09];
+  const position = [35.69439, 51.42151];
   return (
     <div className="w-[80%] h-[450px] border-2 mx-auto">
        <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ width:" 100%",
